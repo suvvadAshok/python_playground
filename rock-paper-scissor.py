270065953e1ed5c8bb043f892ade436cc954538f
ashokadvait <ashok@advaitlabs.com>
2024-04-03 16:06:13 +0530

    add rock, paper, scissor

---
+++ rock-paper-scissor.py
@@ -0,0 +1,48 @@
+import random
+
+gameTerms = ["rock", "paper", "scissor"]
+
+gameStart = True
+
+while gameStart:
+    computer_choice = random.choice(gameTerms)
+
+    human_choice = input("select your choice: ")
+    human_choice = human_choice.lower()
+    if gameTerms.count(human_choice):
+        pass
+    else:
+        print("please..! make correct choice")
+        break
+
+    print(f"computer choice: {computer_choice}")
+
+    if computer_choice == "rock":
+        if human_choice == "paper":
+            print("Wow! you won!")
+            break
+        elif human_choice == "scissor":
+            print("Oooh! you lost")
+            break
+        else:
+            print("ready for rematch")  
+
+    elif computer_choice == "paper":
+        if human_choice == "scissor":
+            print("Wow! you won!")
+            break
+        elif human_choice == "rock":
+            print("Oooh! you lost")
+            break
+        else:
+            print("ready for rematch")
+
+    else:
+        if human_choice == "rock":
+            print("Wow! you won!")
+            break
+        elif human_choice == "paper":
+            print("Oooh! you lost")
+            break
+        else:
+            print("ready for rematch")
